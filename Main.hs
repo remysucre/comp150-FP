1673f5ec21cc87de6ce8261b2826e9321f075e7e
dnunez02 <dan@cs.tufts.edu>
2013-10-26 21:17:22 -0400

    Changed main to produce arbitrary RedBlack trees

--- Main.hs
+++ Main.hs
@@ -1,46 +1,33 @@
 {-# OPTIONS -Wall -Werror -fno-warn-name-shadowing #-}
 
 import qualified Monads.DotWriter as DW
+import qualified Monads.Heap as HP
+import qualified Monads.RedBlack as RB
+import qualified Monads.Rand as Rand
+import Control.Monad (replicateM)
+import System.Random (StdGen, getStdGen)
+import System.Environment (getArgs)
 
-data Tree a = Nil | N { left :: Tree a, val :: a, right :: Tree a } deriving Show
+addColorToTree :: Show a => RB.RBTree a -> DW.Dot ()
+addColorToTree RB.Empty = return ()
+addColorToTree (RB.Node RB.Red l v r) = addColorToTree l >> addColorToTree r >> DW.addColor nv DW.Red >> DW.addStyle nv DW.Bold
+                                        where nv = DW.createNode $ show v
+addColorToTree (RB.Node RB.Black l v r) = addColorToTree l >> addColorToTree r >> DW.addColor nv DW.Black
+                                        where nv = DW.createNode $ show v
 
--- Convert a Tree to a dot string
-treeToDot :: Show a => Tree a -> DW.Dot ()
-treeToDot Nil           = return ()
-treeToDot (N Nil v Nil) = DW.addNode nv >> return ()
-                          where
-                              nv = DW.createNode $ show v
-treeToDot (N l   v Nil) = DW.addNode nv >> treeToDot l >> DW.addNode nl >> DW.addEdge nv nl Nothing
-                          where
-                              nv = DW.createNode $ show v
-                              nl = DW.createNode $ show $ val l
-treeToDot (N Nil v r)   = DW.addNode nv >> treeToDot r >> DW.addNode nr >> DW.addEdge nv nr Nothing
-                          where
-                              nv = DW.createNode $ show v
-                              nr = DW.createNode $ show $ val r
-treeToDot n             = DW.addNode nv >> treeToDot l >> treeToDot r >> DW.addEdge nv nl Nothing >> DW.addEdge nv nr Nothing
-                          where
-                              l  = left n
-                              v  = val n
-                              r  = right n	
-                              nv = DW.createNode $ show v
-                              nl = DW.createNode $ show $ val l
-                              nr = DW.createNode $ show $ val r          
+getRandListLen :: [String] -> Int
+getRandListLen [] = 100
+getRandListLen strs = read $ head strs
 
--- Convert list into a Tree
-listToTree :: [a] -> Tree a
-listToTree as = listToTreeHelper as 0
+printRBTree :: StdGen -> Int -> IO ()
+printRBTree gen len = putStr $ DW.toString $ (HP.heapToDot tree) >> addColorToTree tree
+                      where 
+                          list = fst $ (Rand.escape $ replicateM len Rand.zhe) gen
+                          tree = RB.rbify $ list
 
--- Helper function taking an index
-listToTreeHelper :: [a] -> Int -> Tree a
-listToTreeHelper xs index 
-                 | index >= length xs = Nil
-                 | otherwise          = N l v r
-                                        where
-                                            v = xs !! index
-                                            l = listToTreeHelper xs (2 * index + 1)
-                                            r = listToTreeHelper xs (2 * (index + 1))
--- program 
+-- main program 
 main :: IO ()
-main = let list = take 10 [1..]
-       in putStr $ DW.toString $ treeToDot $ listToTree $ list
+main = do
+         args <- getArgs;
+         gen <- getStdGen;
+         printRBTree gen $ getRandListLen args
