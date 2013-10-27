8b2e81f6b1be44cece8f27866b86a2de9c0cdb35
dnunez02 <dan@cs.tufts.edu>
2013-10-26 21:40:58 -0400

    Added comments. Removed heapToDot.

--- Monads/Heap.hs
+++ Monads/Heap.hs
@@ -1,11 +1,15 @@
 {-# OPTIONS -Wall -Werror -fno-warn-name-shadowing #-}
 
+{-
+    Heap: Typeclass to characterize a tree-like structure, incorrectly
+          named as Heap. Also includes any functions that are necessary
+          for the Heap.
+ 
+          This idea is taken from Andrew Gallant's Dotter monad.
+-}
 module Monads.Heap (
 Heap(..)
-, heapToDot
 ) where
-
-import Monads.DotWriter as DW
 
 -- Idea taken from Andrew Gallant
 class Heap h where
@@ -15,23 +19,3 @@ class Heap h where
     val :: h a -> a
     isEmpty :: h a -> Bool
 
--- Convert a Heap to a dot string
-heapToDot :: (Show a, Heap h) => h a -> DW.Dot ()
---heapToDot empty = return ()
-heapToDot hp 
-           | isEmpty hp = return ()
-           | otherwise =  nodeDot >> leftDot' >> rightDot'
-                          where
-                              (l, r) = (left hp, right hp)
-                              nv = DW.createNode $ show $ val hp
-                              nodeDot = DW.addNode nv
-                              leftDot = heapToDot l
-                              rightDot = heapToDot r
-                   --(leftDot, rightDot) = fmap heapToDot (l, r)
-                   --addChild :: h a -> DW.Dot() -> DW.Dot ()
-                              addChild child childDot = if isEmpty child 
-                                                        then childDot
-                                                        else childDot >> DW.addEdge nv (DW.createNode $ show $ val child) Nothing
-                              leftDot' = addChild l leftDot
-                              rightDot' = addChild r rightDot
-             
