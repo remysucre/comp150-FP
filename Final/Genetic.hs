f22e5eb7c79ef913807f93bf69076bbb1a37df2c
dnunez02 <dan@cs.tufts.edu>
2013-11-26 11:02:57 -0500

    Added timeouts and refactored program to separate compiling and running program

--- Final/Genetic.hs
+++ Final/Genetic.hs
@@ -12,19 +12,39 @@ import System.IO.Unsafe
 import System.Time
 import Data.Bits
 import Data.List (sort)
+import System.Timeout
 import Debug.Trace
 
-data DNA = DNA String String Integer Int deriving Show
-data DNARecord = DR { dna :: DNA, time :: TimeDiff } deriving Show
+{------ General gene class for Genetic Algorithms ------}
 
 class Gene d where
    mutate :: d -> d
    merge :: d -> d -> d
    fitness :: d -> IO TimeDiff
 
+{- 
+   Mutate a gene a certain number of times
+   Return: a gene mutated n times
+-}
 mutateRounds :: Gene d => d -> Int -> d
 mutateRounds gene 0 = gene
-mutateRounds gene x = mutateRounds (mutate gene) (x-1)
+mutateRounds gene n = mutateRounds (mutate gene) (n-1)
+
+{- 
+   Mutate a single gene to multiple genes
+   Return: a list of genes each mutated at least once
+-}
+massMutate :: Gene d => d -> Int -> [d]
+massMutate g i = [g] ++ (take i $ map mutate $ repeat g)
+
+{------ Gene for this Genetic Algorithm ------}
+
+data DNA = DNA { path :: FilePath    -- filePath to program
+               , prog :: String      -- program itself
+               , vec  :: Integer     -- set of bits corresponding to places to insert strictness
+               , n    :: Int         -- upper limit on size of the set of bits
+               }
+           deriving Show
 
 instance Gene DNA where
    mutate = mutateDNA
@@ -36,12 +56,10 @@ instance Eq DNA where
                                                                  (program == program') &&
                                                                  (bits == bits')
 
-instance Eq DNARecord where
-   dr1 == dr2 = (time dr1) == time dr2
-
-instance Ord DNARecord where
-   dr1 <= dr2 = (time dr1) <= time dr2
-
+{- 
+   Mutate a single bit of the DNA
+   Return: new piece of DNA that has been mutated
+-}
 mutateDNA :: DNA -> DNA
 mutateDNA (DNA fp program bits numPlaces) = writeDNAToDisk d'
                                   where 
@@ -50,6 +68,29 @@ mutateDNA (DNA fp program bits numPlaces) = writeDNAToDisk d'
                                       numPlaces' = if numPlaces < i then i else numPlaces
                                       d' = DNA fp program' bits' numPlaces'
 
+{- 
+   Mutate DNA according to a set of bits of a given size
+   Return: new piece of DNA mutated according to bits
+-}
+mutateDNAWithSet :: Integer -> Int -> DNA -> DNA
+mutateDNAWithSet bits size d = mutateDNAWithSetH d bits size 0
+
+{- 
+   Helper function: Goes through the bits and mutates DNA accordingly
+-}
+mutateDNAWithSetH :: DNA -> Integer -> Int -> Int -> DNA
+mutateDNAWithSetH d@(DNA fp prog oldBits np) bits numPlaces i = if i == (numPlaces)
+                                                               then writeDNAToDisk $ DNA fp prog bits numPlaces
+                                                               else mutateDNAWithSetH d' bits numPlaces (i+1)
+                                                               where
+                                                                  d' = if not $ (testBit bits i) == (testBit oldBits i)
+                                                                       then DNA fp (flipBang fp prog i) oldBits np
+                                                                       else d
+
+{- 
+   Merge two parents to create a single child
+   Return: new piece of DNA that results from both parents
+-}
 mergeDNA :: DNA -> DNA -> DNA
 mergeDNA d1@(DNA fp program bits np) d2@(DNA fp' program' bits' np') = mutateDNAWithSet bits'' np'' d'
                                                                        where
@@ -57,6 +98,10 @@ mergeDNA d1@(DNA fp program bits np) d2@(DNA fp' program' bits' np') = mutateDNA
                                                                            np'' = if np < np' then np' else np
                                                                            d' = DNA fp program bits'' np''
 
+{- 
+   Helper function: Writes a program in DNA to a temp file and updates DNA
+   Return: DNA updated to point to file just written on disk
+-}
 writeDNAToDisk :: DNA -> DNA
 writeDNAToDisk (DNA fp program bits numPlaces) = DNA fp' program bits numPlaces
                                                  where
@@ -67,53 +112,137 @@ writeDNAToDisk (DNA fp program bits numPlaces) = DNA fp' program bits numPlaces
                                                                 return name
                                                      fp' = dropExtension $ unsafePerformIO action
 
-mutateDNAWithSetH :: DNA -> Integer -> Int -> Int -> DNA
-mutateDNAWithSetH d@(DNA fp prog oldBits np) bits numPlaces i = if i == (numPlaces)
-                                                               then writeDNAToDisk $ DNA fp prog bits numPlaces
-                                                               else mutateDNAWithSetH d' bits numPlaces (i+1)
-                                                               where
-                                                                  d' = if not $ (testBit bits i) == (testBit oldBits i)
-                                                                       then DNA fp (flipBang fp prog i) oldBits np
-                                                                       else d
+{-
+   Compile a program and return the exit code. Also deletes all temporary files.
+-}
+compile :: FilePath -> IO ExitCode
+compile fp = do
+               exit <- system $ "ghc -XBangPatterns -outputdir temp/ -o " ++ fp ++ " " ++ fp ++ ".hs"
+               system $ "rm temp/*"
+               return exit
 
-mutateDNAWithSet :: Integer -> Int -> DNA -> DNA
-mutateDNAWithSet bits size d = mutateDNAWithSetH d bits size 0
-
+{-
+   Test fitness of DNA 
+   Returns: infTime if there are any errors
+            run time of the executable otherwise
+-}
 fitnessDNA :: DNA -> IO TimeDiff
 fitnessDNA (DNA fp program _ _) = do
-                                    staticExit <- system $ "ghc -XBangPatterns -outputdir temp/ -o " ++ fp ++ " " ++ fp ++ ".hs"
-                                    start <- getClockTime
-                                    dynExit <- system $ "./" ++ fp
-                                    end <- getClockTime
+                                    --staticExit <- compile fp
+                                    time <- timeIO $ system $ "./" ++ fp
                                     system $ "rm " ++ fp
-                                    system $ "rm temp/*"
-                                    case (staticExit, dynExit) of
-                                         (ExitSuccess, ExitSuccess)   -> return $ diffClockTimes end start
-                                         otherwise                    -> return $ TimeDiff {tdYear = maxBound, tdMonth = maxBound, tdDay = maxBound
-                                                                                           ,tdHour = maxBound, tdMin = maxBound, tdSec = maxBound
-                                                                                           ,tdPicosec = 0}
+                                    return time
+                                    --case staticExit of
+                                    --    ExitSuccess   -> return time
+                                    --    ExitFailure _ -> return infTime
 
+{-
+   Create a new strand of DNA using a file path and a program
+-}
 -- TODO : go through program and build the bit vector
 createDNA :: String -> String -> DNA
 createDNA fp program = DNA fp program 0 $ placesToStrict fp program
 
-massMutate :: Gene d => d -> Int -> [d]
-massMutate g i = [g] ++ (take i $ map mutate $ repeat g)
+{------ Records used to sort results of fitness ------}
 
+{- 
+   A record for a given piece of DNA after fitness 
+-}
+data DNARecord = DR { dna :: DNA, time :: TimeDiff } deriving Show
+
+instance Eq DNARecord where
+   dr1 == dr2 = (time dr1) == time dr2
+
+instance Ord DNARecord where
+   dr1 <= dr2 = (time dr1) <= time dr2
+
+{-
+   Create a record based off of DNA and the result of fitness function
+-}
 createDNARecords :: DNA -> TimeDiff -> DNARecord
 createDNARecords d time = DR d time
+
+{------ Functions for dealing with TimeDiffs ------}
+
+{- 
+   This denotes infinite time for nonterminating or failed computations
+-}
+infTime :: TimeDiff
+infTime = TimeDiff {tdYear = maxBound, tdMonth = maxBound, tdDay = maxBound
+                   ,tdHour = maxBound, tdMin = maxBound, tdSec = maxBound
+                   ,tdPicosec = 0}
+
+{- 
+   Converts TimeDiff to an Int up to the hours
+-}
+timeToInt :: TimeDiff -> Int
+timeToInt time = (secToMicro $ minToSec $ hourToMin $ tdHour time) + 
+                 (secToMicro $ minToSec $ tdMin time) + 
+                 (secToMicro $ tdSec time) + 
+                 (picoToMicro $ tdPicosec time)
+
+{------ Functions to convert to Microseconds ------}
+secToMicro :: Int -> Int
+secToMicro sec = sec * 1000
+
+picoToMicro :: Integer -> Int
+picoToMicro pico = fromInteger $ quot pico 1000000
+
+minToSec :: Int -> Int
+minToSec minute = minute * 60
+
+hourToMin :: Int -> Int
+hourToMin hour = hour * 60
+
+{- 
+   Timeout the fitness function after "time" microseconds
+   Return: infTime if the computation fails or times out
+           run time if the executable otherwise
+-}
+fitnessWithTimeout :: (Gene d) => Int -> d -> IO TimeDiff
+fitnessWithTimeout time gene = do
+                                 result <- timeout time $ fitness gene
+                                 case result of
+                                     Nothing -> return infTime
+                                     Just t  -> return t
+
+{-
+   Takes an action with an exit code and times it
+   Return: infTime if the computation fails
+           run time of the action otherwise
+-}
+timeIO :: IO ExitCode -> IO TimeDiff
+timeIO action = do
+                  start <- getClockTime
+                  exit <- action
+                  end <- getClockTime
+                  case exit of
+                      ExitSuccess   -> return $ diffClockTimes end start
+                      ExitFailure _ -> return infTime
+
+{- 
+   Number to add to the base time for timeout (for compilation)
+-}
+epsilon :: Int
+epsilon = secToMicro 5
+
+{------ Main algorithm ------}
 
 main :: IO ()
 main = do 
           program <- readFile filePath
-          def <- return $ createDNA "Main" program
-          time <- fitness $ def
-          dnas <- return $ massMutate def 5
-          times <- sequence $ map fitness dnas
+          def <- return $ createDNA moduleName program
+          start <- getClockTime
+          fitness $ def
+          end <- getClockTime
+          time <- return $ diffClockTimes end start
+          dnas <- return $ massMutate def numMutations
+          sequence $ map (compile . path) dnas -- Compile all the programs
+          times <- sequence $ map (fitnessWithTimeout $ (timeToInt time) + epsilon ) dnas
           records <- return $ sort $ map (uncurry createDNARecords) $ zip dnas times
-          print records
+          print $ records
        where
            bits = 33 :: Integer
+           moduleName = "Main"
            filePath = "Main" ++ ".hs"
-
--- 3) terminate if time exceeds base "System.Timeout"
+           numMutations = 5
