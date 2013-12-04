3a2374bf7988ca07cfd873d7c3e1bccb549cf650
dnunez02 <dan@cs.tufts.edu>
2013-12-04 15:11:22 -0500

    Fully supporting shell script for timing

--- Final/timer.sh
+++ Final/timer.sh
@@ -3,15 +3,16 @@
 PROG=$1
 TIME=$3
 REPS=$2
+FILE=$4
 
-rm -f test.txt
-
+#rm -f test.txt
+rm -f ${FILE}
 for (( i=1; i<=$REPS; i++ ))
     do
-    touch test.txt
-    timeout ${TIME} time -f %e ${PROG} > /dev/null 2>> test.txt
+    touch ${FILE}
+    timeout 2h time -f %U -a -o ${FILE} ${PROG} +RTS -N4 -RTS 10 > /dev/null
     VAL=$?
     if [ "${VAL}" -eq "124" ]
-    then echo "-1.0" >> test.txt
+    then echo "-1.0" >> ${FILE}
     fi
 done
