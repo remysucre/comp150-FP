7bf1425f718a6d426f0afcae2a97b221cdbea571
Yisu Wang <ywang30@cs.tufts.edu>
2015-07-07 11:37:07 -0400

    finish partition & add log

--- benchmark/MoveIfStrict.hs
+++ benchmark/MoveIfStrict.hs
@@ -4,6 +4,7 @@ import System.Directory
 import System.Environment
 import Data.Functor
 import Control.Applicative
+import Control.Cond
 import System.FilePath.Find
 import Control.Monad.IfElse
 import HasStrict.HasStrict
@@ -11,7 +12,14 @@ import HasStrict.HasStrict
 moveTo :: FilePath -> FilePath -> IO()
 moveTo dir dir' = renameDirectory dir $ dir' ++ "/" ++ dir
 
-main = do 
-    dir <- head <$> getArgs
-    --if hasStrict dir then dir `moveTo` "strictapps" else return ()
-    whenM (hasStrict dir) (dir `moveTo` "strictapps")
+main = do {
+    dir <- head <$> getArgs;
+    ifM (hasStrict dir)
+        (do {
+            dir `moveTo` "strictapps";
+            putStrLn $ "package " ++ dir ++ " has bang"
+        })
+        (do {
+            putStrLn $ "package " ++ dir ++ " has no bang"
+        })
+    }
