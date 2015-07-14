bf9142a1f4ecb58194418a1a4eee8e2c3bf51fa5
Yisu Wang <ywang30@cs.tufts.edu>
2015-07-14 12:52:43 -0400

    clean

--- BMain.hs
+++
@@ -1,8 +0,0 @@
-{- Parts: 
- - Cabal parsers: get pop libs
- - GetApp: download apps from hackage
- - LargePacks: look for large packages
- - HasStrict: recognize strict packs/files
- - Partition: separates strict sources from non-strict ones
- - -}
-
