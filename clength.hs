3bfe18bccbbc52141d2f6a62d1d9b678ee0ee2d6
Remy <remy.sucre@gmail.com>
2015-07-21 10:54:19 -0400

    clean

--- clength.hs
+++
@@ -1,8 +0,0 @@
-clength xs = length' xs 0
-  where length' []     n = n
-        length' (x:xs) n = length' xs $! (n + 1)
-
-len = clength [1..1000000]
-
-main = 
-    putStrLn $ show len
--- gentest/clength.hs
+++ gentest/clength.hs
@@ -2,7 +2,7 @@ clength xs = length' xs 0
   where length' []     n = n
         length' (x:xs) n = length' xs $! (n + 1)
 
-len = length [1..1000000]
+len = clength [1..1000000]
 
 main = 
     putStrLn $ show len
