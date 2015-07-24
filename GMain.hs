86549c841b329f1f67f254db8198d865473f6f7c
remy <remy.sucre@gmail.com>
2015-07-24 11:50:47 -0400

    BEFORE APOCALYPES

--- GMain.hs
+++ GMain.hs
@@ -9,16 +9,14 @@ import Debug.Trace
 
 main :: IO ()
 main = do 
-          _ <- createDirectoryIfMissing True "files"
-          def <- createGeneFromFile filePath {- create initial gene, which contains original source and no Bang flipped. -}
-          _ <- compile $ path $ head $ getStrand def
+          createDirectoryIfMissing True "files"
+          gene0 <- createGeneFromFile filePath {- create initial gene, which contains original source and no Bang flipped. -}
+          _ <- compile $ path $ head $ getStrand gene0
           print "Obtaining base time of program"
-          -- !time' <- fitness fitnessRuns (fromInteger . toInteger $ (maxBound :: Int)) def
-          !time' <- fitness fitnessRuns 100 def
-          --time <- return $ if time' < 0.0 then (fromInteger . toInteger $ (maxBound :: Int)) else time'
+          !time' <- fitness fitnessRuns 100 gene0
           let time = if time' < 0.0 then 100 else time'
           print $ "Base time is " ++ (show time)
-          dnas <- trace (show time) $ geneticAlg [def] runs time fitnessRuns poolSize ((GR def time), 0) emptyGeneDict
+          dnas <- trace (show time) $ geneticAlg [gene0] runs time fitnessRuns poolSize ((GR gene0 time), 0) emptyGeneDict
           -- TRACE IS IMPURE
           print $ "Best found: " ++ (show $ getStrand $ head dnas)
         where
