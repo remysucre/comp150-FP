6209d6e75c4687a88c12aa9947ad7f5f7381531b
remy <remy.sucre@gmail.com>
2015-07-27 15:12:50 -0400

    tread

--- profile/fib/fix/fibsum1.hs
+++ profile/fib/fix/fibsum1.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE BangPatterns #-}
 import Data.List
 
-fib 0 (_, b)  = b
-fib n (!a, !b) = fib (n - 1) (b, a + b)
+fib 0 _ b  = b
+fib n !a !b = fib (n - 1) b (a + b)
 
-fiba = fib 500000 (0, 1)
+fiba = fib 500000 0 1
 
 main = do
     putStrLn $ show fiba
